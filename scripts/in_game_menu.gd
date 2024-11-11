deff8765b0e3262f2f144af947beb178e20fdd96
HeFas <art.aparin2016@gmail.com>
2024-11-11 19:14:08 +0300

    Update_19

--- scripts/in_game_menu.gd
+++ scripts/in_game_menu.gd
@@ -20,3 +20,16 @@ func _on_button_exit_pressed() -> void:
 	global.Save()
 	get_tree().change_scene_to_file("res://nodes/worlds/main_menu.tscn")
 	
+
+func _on_continue_m_entered() -> void:
+	$ContinueBtn.self_modulate = Color(1,1,1,0.7)
+
+func _on_quite_m_entered() -> void:
+	$QuiteBtn.self_modulate = Color(1,1,1,0.7)
+
+
+func _on_continue_m_exited() -> void:
+	$ContinueBtn.self_modulate = Color(1,1,1,1)
+
+func _on_quite_m_exited() -> void:
+	$QuiteBtn.self_modulate = Color(1,1,1,1)
