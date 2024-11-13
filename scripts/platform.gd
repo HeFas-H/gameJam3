c7252d78e91ceccf198bc6fb604279410e54b998
HeFas <art.aparin2016@gmail.com>
2024-11-13 09:02:12 +0300

    platform_delta_fix

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 
 @onready var pos = get_meta("point_1") + position
 #var dir = true
-var deltatime = 2
+var deltatime = 200
 
 func _ready() -> void:
 	set_meta("point_1", get_meta("point_1") + position )
 	set_meta("point_2", get_meta("point_2") + position )
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if self.position != pos:
-		position = Vector2(move_toward(self.position.x, self.pos.x, deltatime), move_toward(self.position.y, self.pos.y, deltatime))
+		position = Vector2(move_toward(self.position.x, self.pos.x, deltatime*delta), move_toward(self.position.y, self.pos.y, deltatime*delta))
 	else:
 		if pos == get_meta("point_1"):
 			pos = get_meta("point_2")
